9ee038ba7c1c15fbdfa7b4dd80d061b24755332f
iesus <iesus.calvillo@gmail.com>
2020-04-02 11:36:25 -0400

    .gitignore config.py

---
+++ src/config.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 26, 2020
+
+Here we store all global variables accross the project and that might change from one environment to another
+'''
+project_folder="/Users/jesus/Documents/EclipseWorkspace/classinsight-language-git"
+
+data_folder=project_folder+"/Data"
+tf_cache_folder=project_folder+"/tf_cache"
+saved_models_folder=project_folder+"/saved_models"
+plots_folder=project_folder+"/plots"
+
+csv_folder=data_folder+"/2_CSV_Files"
+json_folder=data_folder+"/3_JSON_Files"
+datasets_folder=data_folder+"/4_Datasets"
+test_folder=data_folder+"/5_Test"
+
